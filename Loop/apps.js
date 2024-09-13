0a23ae04c99f1e29dda4e73300444ae4c046be6b
imon <imonsorder106@gmail.com>
2024-09-13 18:38:01 +0600

    save

--- Loop/apps.js
+++ Loop/apps.js
@@ -2,6 +2,10 @@
 //      For Loop
 // =========================
 
+// EXAMPLE FOR LOOP
+
+// 1 //
+
 // let users = ["Imon","Rashid","Sobuj","Shohidul","Arian","Rahman"];
 
 // let arrLength = users.length;
@@ -10,6 +14,8 @@
 //     console.log(users[counter]);
 
 // }
+
+// 2 //
 
 // let num = 10;
 
@@ -21,10 +27,48 @@
 //   }
 // }
 
+// 3 //
+
 // const bikes = ["Honda", "Yamaha", "Suzuki", "Bajaj"];
 // for (i = 1; i < bikes.length; i++) {
 //   console.log(bikes[i]);
 // }
+
+// TIPS
+// ami caile expression 1 loop er baire likhte pari
+
+// let i = 0;
+// for (; i < 10; i++) {
+//   console.log(i);
+// }
+
+// ami caile expression 2 loop e nao dite pari... but loop ta off korar jonno akta break dite hobe...
+
+// for (let i = 0; ; i++) {
+//   if (i == 10) {
+//     break;
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+// ami caile expression 3 ta block er moddheo dite pari
+// for (let i = 0; i < 10; ) {
+//   console.log(i);
+//   i++;
+// }
+
+// =========================
+//      For IN Loop
+// =========================
+
+// FOR IN LOOP USE only for object
+
+const person = { fname: "Imon", lname: "Sorder", age: "25" };
+
+for (let x in person) {
+  console.log(person[x]);
+}
 
 // While Loop.....
 
