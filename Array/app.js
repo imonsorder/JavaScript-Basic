3d6f7e9828a7972fc8cdd733359ab2de16b5136d
Imon <imonsorder106@gmail.com>
2024-10-16 18:06:08 +0600

    Array akta file kora holo

---
+++ Array/app.js
@@ -0,0 +1,17 @@
+let names = ["Shakib", "Tamim", "Musfiq"];
+let newNames = [];
+
+for (let i = 0; i < names.length; i++) {
+  newNames.push("Mr " + names[i]);
+}
+
+console.log(newNames);
+// newNames[] e notun akta array kora holo sathe MR diye....
+
+let nums = [1, 2, 3, 4, 5, 6];
+let newNums = [];
+for (let i = 0; i < nums.length; i++) {
+  newNums.push(nums[i] * 2);
+}
+// newNums[] e notun akta arry kora holo agertar double number diye.....
+console.log(newNums);
