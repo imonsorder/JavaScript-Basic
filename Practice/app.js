52ec7520c87b2c9b3ed7670f4c8feee71b42bc8d
imon <imonsorder106@gmail.com>
2024-10-21 21:34:11 +0600

    practice file added

---
+++ Practice/app.js
@@ -0,0 +1,26 @@
+// let person = "Imon Sorder";
+// let newPerson = person[1];
+// console.log(newPerson);
+
+const numbers = [3, 6, 11, 49, 65, 70];
+
+// numbers.forEach(show);
+// function show(el) {
+//   console.log(el + 6);
+// }
+
+// numbers.forEach((el) => {
+//   console.log(el + 6);
+// });
+
+let bike = {
+  Brand: "Honda",
+  Model: "Hornet",
+  CC: 160,
+  Color: "Red",
+  brandModel() {
+    console.log("");
+  },
+};
+
+console.log(bike.Brand);
