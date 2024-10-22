d0303c46b18d28b60a0823af09f20475b8a484f0
Imon <imonsorder106@gmail.com>
2024-10-22 20:52:00 +0600

    prace file edited

--- Practice/app.js
+++ Practice/app.js
@@ -2,7 +2,7 @@
 // let newPerson = person[1];
 // console.log(newPerson);
 
-const numbers = [3, 6, 11, 49, 65, 70];
+// const numbers = [3, 6, 11, 49, 65, 70];
 
 // numbers.forEach(show);
 // function show(el) {
@@ -13,14 +13,44 @@ const numbers = [3, 6, 11, 49, 65, 70];
 //   console.log(el + 6);
 // });
 
-let bike = {
-  Brand: "Honda",
-  Model: "Hornet",
-  CC: 160,
-  Color: "Red",
-  brandModel() {
-    console.log("");
-  },
-};
+// let bike = {
+//   Brand: "Honda",
+//   Model: "Hornet",
+//   CC: 160,
+//   Color: "Red",
+//   Sold: [50, 65, 20, 35, 27, 16],
+//   brandModel() {
+//     console.log(`The bike name ${this.Brand}\nImon ${this.Model}`);
+//   },
+// };
 
-console.log(bike.Brand);
+// bike.brandModel();
+// bike.showCC();
+// console.log(bike.Brand);
+// bike.brandModel();
+
+// function user(el) {
+//   let username = "Imon Sorder";
+//   let that = 20 * el;
+//   console.log(that);
+// }
+
+// let value = user(20);
+// console.log(value);
+
+// =============== string method  ========
+let m =
+  "Cow are domestic animal. We eats cow's milk. this cow are beutiful. Cow are red";
+let x = " Hello this new Bangladesh";
+let y = "Imon";
+let z = 5;
+
+m = m.replaceAll("Cow", "Goat");
+m = m.replaceAll("cow", "goat");
+// let value = x.replace(/banglAdesh/i, "India"); // string change kora jay.. /bangladesh/i aita dile case insensitive hoye jay..
+// let value = x.repeat(3); kono string k koybar print korbe ta bola jay.. vitore 3 lekha tai 3 bar hobe..
+// let value = num.toString(); // number k string kore...
+// let value = x.trim(); // kono string er suru r ses er white space bad dey..
+// let value = x.concat(" ", y);  // x and y duita string k add kore diche..
+
+console.log(m);
