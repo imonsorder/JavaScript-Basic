3c4bb533f6dc05e4b1ca0177a724e389e58ca928
imon <imonsorder106@gmail.com>
2024-10-25 17:25:38 +0600

    new file stringMethod added

---
+++ StringMethod/app.js
@@ -0,0 +1,45 @@
+//==============================================
+// All string methods return a new value.
+// They do not change the original variable.
+//==============================================
+
+//============================Varible for below string method===================
+let m =
+  "Cow are domestic animal. We eats cow's milk. this cow are beutiful. Cow are red";
+let x = "Hello this new Bangladesh";
+let y = "Imon";
+let z = "5";
+// =======================================================================================
+// m = m.replaceAll("Cow", "Goat");
+// m = m.replaceAll("cow", "goat");
+// let value = x.replace(/banglAdesh/i, "India"); // string change kora jay.. /bangladesh/i aita dile case insensitive hoye jay..
+// let value = x.repeat(3); kono string k koybar print korbe ta bola jay.. vitore 3 lekha tai 3 bar hobe..
+// let value = num.toString(); // number k string kore...
+// let value = x.trim(); // kono string er suru r ses er white space bad dey..
+// let value = x.concat(" ", y);  // x and y duita string k add kore diche..
+// let value = x.substr(2,6);  // same to slice but difference is second value .. 6 means 6 word cutting korbe..
+// let value = x.slice(-10); // negative value dile ulta theke suru korbe..
+// let value = x.slice(6); // slice(start) akta value dile 6 index porjonto sob word bad diye tarpor theke suru korbe...
+// let value = x.slice(6, 11); // slice(start, end) duita value dile 1st suru korbe and 2nd ta diye ses korbe...
+// y.at(1) // accesing 2nd word...
+// let value = y.toUpperCase();
+// let value = y.toLowerCase();
+// let value = z.padStart(4, "x");  // z er valur age 3bar x print hobe.... number e kaj korbe na ata.. number kaj korte caile toString e convert kore nite hobe
+// let value = z.padEnd(4, "x"); // z er valur por 3bar x print hobe....
+
+//====================
+//  string to array  //
+//====================
+
+/**
+ *  string k caile array banano jay..
+ * split() method er vitore kichu na dile pura ta array banay dibe
+ * split() er vitore ja dibo seta bad diye se array banay dibe..
+ * string e (,) coma thakle coma split() er moddhe dibo..
+ * r space thake space (' ') split er moddhe dibo..
+ */
+// let word = "Imon for Sumon";
+// let remove = word.split(" ");
+// console.log(remove);
+
+console.log("imon");
