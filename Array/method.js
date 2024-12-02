ed38ede6e3201589fcb4d6a1fe1c5de2624c5b72
Imon <imonsorder106@gmail.com>
2024-12-02 20:23:03 +0600

    to file

--- Array/method.js
+++ Array/method.js
@@ -22,9 +22,61 @@
  * pass kora hoy.. (acc,el, idx, arr)
  * accumulator => sob gulo aksathe joro kora
  */
-let arr = [1, 2, 3];
-let result = arr.reduce(function (acc, el) {
-  return (acc += el);
-}, 0);
+// let arr = [1, 2, 3];
+// let result = arr.reduce(function (acc, el) {
+//   return (acc += el);
+// }, 0);
 
-console.log(result);
+// console.log(result);
+
+const characters = [
+  {
+    name: "Luke Skywalker",
+    height: "172",
+    mass: "77",
+    eye_color: "blue",
+    gender: "male",
+  },
+  {
+    name: "Darth Vader",
+    height: "202",
+    mass: "136",
+    eye_color: "yellow",
+    gender: "male",
+  },
+  {
+    name: "Leia Organa",
+    height: "150",
+    mass: "49",
+    eye_color: "brown",
+    gender: "female",
+  },
+  {
+    name: "Anakin Skywalker",
+    height: "188",
+    mass: "84",
+    eye_color: "blue",
+    gender: "male",
+  },
+];
+
+// Get the total mass of all characters
+
+// let totalMass = characters.reduce(function (acc, el) {
+//   return (acc += Number(el.mass));
+// }, 0);
+// console.log(totalMass);
+
+// Get the total height of all characters
+
+// Get the total number of characters in all the character names
+// let totalChar = characters.reduce(function (acc, el) {
+//   return (acc += el.name.length);
+// }, 0);
+// console.log(totalChar);
+//=============================
+// let str = "Hello World";
+// let newStr = str.split(" ").join(""); // charecter er majher space bad dite aituk kora jay....
+// console.log(newStr);
+
+// Get the total number of characters by eye color (hint. a map of eye color to count)
