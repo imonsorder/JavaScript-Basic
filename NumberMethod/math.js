ee3bb8685563d589ce9e035c2791da8175f98e35
Imon <imonsorder106@gmail.com>
2024-12-04 18:05:41 +0600

    math.js file added

---
+++ NumberMethod/math.js
@@ -0,0 +1,7 @@
+Math.abs(-50); // negative value dile output positive value asbe...
+Math.ceil(1.1); // decimal number ta porer akta purno songkha dey.. jodi negative thake taile decimal songkha thakle sei songkhar purno sonkha bose...
+Math.floor(2.9); // ata ceil er biporit..
+Math.round(2.4); // ata purno songkha 2 return korbe.. decimal songkha 5 or 5 er beshi hole 3 purno songkha return korbe..
+Math.random(); // 1 er theke nice random akta number genarate korbe..
+Math.max(4, 6, 38, 700); // sorbocco number ta return korbe..
+Math.pow(3, 2); // 3 power 2.. return korbe 3*3 = 9;
